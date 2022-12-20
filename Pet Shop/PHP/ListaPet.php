21e7ca3202172c5a8a1b709698308cd652791287
Melissa <91746003+melpompom@users.noreply.github.com>
2022-12-19 21:53:53 -0300

    Add files via upload

---
+++ Pet Shop/PHP/ListaPet.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+        crossorigin="anonymous"></script>
+    <title>Lista de Pets</title>
+
+    <script src="scriptPet.js"></script>
+</head>
+
+<body>
+<body class='container'>
+    <h3>Listas</h3>
+    <section id='cadpet' class='row'>
+    <form action="GET" id="pesquisa">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Pesquisar..." id="busca">
+            <p>
+            <input class="btn btn-cancel" type="submit" name="pesquisa" value="Pesquisar">
+        </div>
+    </form>
+    <table class="table table-striped table-hover">
+    <div class='row'>
+                <!-- aqui montamos a tabela com os dados vindo do banco -->
+                <table class='table table-striped table-hover'>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nome do Pet</th>
+                            <th>Tipo de Pet</th>
+                            <th>Sexo</th>
+                            <th>Dono</th>
+                            <th>Editar</th>
+                            <th>Excluir</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider" id='corpo'>
+                    
+                    </tbody>      
+    </table>
+    </div>
+
+</body>
+
+</html>
---
+++ Pet Shop/PHP/scriptDono.js
@@ -0,0 +1,37 @@
+
+        // floreio -- para o usuário confirmar a exclusão
+        function excluir(url){
+            if (confirm("Confirma a exclusão?"))
+                window.location.href = url; //redireciona para o arquivo que irá efetuar a exclusão
+        }
+
+        window.onload = (function (){
+            carregaDados();
+            document.getElementById('fpesquisa').addEventListener('submit',function(ev){
+                ev.preventDefault();
+                carregaDados();
+            });
+            document.getElementById('busca').addEventListener('keyup',carregaDados);
+        });
+
+        function carregaDados(){
+            busca = document.getElementById('busca').value;
+            const xhttp = new XMLHttpRequest();  // cria o objeto que fará a conexão assíncrona
+            xhttp.onload = function() {  // executa essa função quando receber resposta do servidor
+                dados = JSON.parse(this.responseText); // os dados são convertidos para objeto javascript
+                montaTabela(dados);
+            }
+            // configuração dos parâmetros da conexão assíncrona
+            xhttp.open("GET", "pesquisa.php?busca=" + busca, true);  // arquivo que será acessado no servidor remoto  
+            xhttp.send(); // parâmetros para a requisição
+
+        }
+        function montaTabela(dados){
+            str = "";
+            for(Dono of dados){
+                editar = '<a href=cadDono.php?acao=editar&id='+Dono.id+'>Alt</a>';
+                excluir = "<a href='#' onclick=excluir('acaoDono.php?acao=excluir&id="+Dono.id+"}')>Excluir</a>";
+                str += "<tr><td>"+Dono.id+"</td><td>"+Dono.nome+'</td><td>'+Dono.telefone+'</td><td>'+Dono.Endereco+'</td><td>'+editar+'</td><td>'+excluir+'</td></tr>';
+            }
+            document.getElementById('corpo').innerHTML = str;
+        }
